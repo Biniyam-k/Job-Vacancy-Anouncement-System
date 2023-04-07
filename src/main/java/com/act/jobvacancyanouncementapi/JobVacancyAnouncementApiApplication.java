853fbfa6d797a616d8afe2fbb11730fae6b9872f
Biniyam Kabtamu <biniyamkabtamu@gmail.com>
2023-04-07 20:05:47 +0300

    Job vacancy anouncement api set up

---
+++ src/main/java/com/act/jobvacancyanouncementapi/JobVacancyAnouncementApiApplication.java
@@ -0,0 +1,14 @@
+package com.act.jobvacancyanouncementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobVacancyAnouncementApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JobVacancyAnouncementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/act/jobvacancyanouncementapi/JobVacancyAnouncementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.act.jobvacancyanouncementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobVacancyAnouncementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
